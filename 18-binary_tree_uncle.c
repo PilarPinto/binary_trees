112b518bf9169e82131adf0bb9daf7dc6e8e1561
PilarPinto <piapintoch@unal.edu.co>
2020-03-11 22:09:15 +0000

    [Test]-Change conditions order

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -6,10 +6,10 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (!node || !node->parent->parent || !node->parent)
+	if (!node || !node->parent || !node->parent->parent)
 		return (NULL);
-	if (node->parent->parent->right == node->parent)
-		return (node->parent->parent->left);
-	else
+	if (node->parent->parent->left == node->parent)
 		return (node->parent->parent->right);
+	else
+		return (node->parent->parent->left);
 }
