17e072492a5623218546aeed2d5da1c8291a8bfe
PilarPinto <piapintoch@unal.edu.co>
2020-03-11 22:35:05 +0000

    [Test]-Adding NULL condition

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -9,6 +9,10 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *n_node = malloc(sizeof(binary_tree_t));
 
+	if (!parent)
+		return (NULL);
+	n_node = malloc(sizeof(binary_tree_t));
+
 	if (!n_node)
 	{
 		return (NULL);
