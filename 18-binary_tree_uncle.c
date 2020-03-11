82bc06f1f3187100fbb99c6974a62dbfe1ce42cf
PilarPinto <piapintoch@unal.edu.co>
2020-03-11 21:51:22 +0000

    [Feature]-function that finds the uncle of a node

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_uncle-finds the uncle of a node
+ * @node:Is the node that will be cheked
+ * Return: The uncle node
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (!node || !node->parent->parent ||
+		!node->parent->parent->right || !node->parent->parent->left)
+		return (0);
+	if (node->parent->parent->right == node->parent)
+		return (node->parent->parent->left);
+	else
+		return (node->parent->parent->right);
+}
